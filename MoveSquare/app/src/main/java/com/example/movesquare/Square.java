f233e7cfa4526a61277b573937b7ec2ca7dce304
Shashvat <68447474+smehtacoder@users.noreply.github.com>
2022-07-08 21:41:37 -0700

    Added files

---
+++ MoveSquare/app/src/main/java/com/example/movesquare/Square.java
@@ -0,0 +1,18 @@
+package com.example.movesquare;
+
+public class Square {
+    /*CODE:
+        -Use robot.startDriveToPosition(Speed, Distance)
+        -Use robot.startStrafeToPosition(Speed, Distance)
+        -Max power you can use is 0.5, as we don't want the robot crashing into anything
+        -Use distance from 30 CM to 50 CM
+        -To move backwards use a negative distance
+        -startDriveToPosition and startStrafeToPosition and defined functions in last years code,
+         so don't worry about errors
+        -When we test, you will slack us the code, and we will add your code to our copy so we can
+         test it
+     */
+
+    //Write Code here
+    
+}
